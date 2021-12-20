72b4ed5f63af586c1082553cc43df80d053ba1fe
amyria3 <54778894+amyria3@users.noreply.github.com>
2021-12-20 19:12:54 +0100

    Add files via upload

---
+++ css_and_js/script.js
@@ -0,0 +1,11 @@
+function portfolio_js_function() {
+    var elmnt = document.getElementById("portfolio");
+    elmnt.scrollIntoView();
+}
+function card_aboutMe_js_function() {
+    var elmnt = document.getElementById("card_aboutMe");
+    elmnt.scrollIntoView();
+}
+
+
+
